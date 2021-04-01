cd20839678e693d2927515da17911b36e3c3d148
Christi-Lee-MC <79948400+Christi-Lee-C@users.noreply.github.com>
2021-04-01 13:38:25 -0700

    added console log to test and variables for the character selections

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,25 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var pwdLength = parseInt (prompt ("How long would you like your password to be?"));
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var numChars = "1234567890";
+var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-  if (pwdLength > 8 && pwdLength < 128) {
+function generatePassword() {
+  var passwordLength = parseInt (prompt("How long would you like your password to be?"));
+
+  if (passwordLength > 8 && passwordLength < 128) {
+    var includeUpper = confirm("Would you like to include UPPERCASE characters?");
+    var includeLower = confirm("Would you like to include lowercase characters?");
+    var includeNumbers = confirm("Would you like to include numeric characters?");
+    var includeSpecial = confirm("Would you like to include special characters?");
+
+    console.log(includeUpper);
+    console.log(includeLower);
+    console.log(includeNumbers);
+    console.log(includeSpecial);
+    
     } else {
       alert("Please enter a length between 8 and 128");
     }
