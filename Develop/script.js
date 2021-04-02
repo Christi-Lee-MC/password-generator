301aa46a2d44ec10aa2ab2ebdf98ae63819a1f88
Christi-Lee-MC <79948400+Christi-Lee-C@users.noreply.github.com>
2021-04-01 18:54:08 -0700

    added is statements to build characterSet

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,62 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerChars = "abcdefghijklmnopqrstuvwxyz";
 var numChars = "1234567890";
-var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+var characterSet = "";
 
 function generatePassword() {
-  var passwordLength = parseInt (prompt("How long would you like your password to be?"));
+  var passwordLength = parseInt(
+    prompt(
+      "How long would you like your password to be? Pick a number between 8 and 128"
+    )
+  );
 
   if (passwordLength > 8 && passwordLength < 128) {
-    var includeUpper = confirm("Would you like to include UPPERCASE characters?");
-    var includeLower = confirm("Would you like to include lowercase characters?");
-    var includeNumbers = confirm("Would you like to include numeric characters?");
-    var includeSpecial = confirm("Would you like to include special characters?");
+    var includeUpper = confirm(
+      "Would you like to include UPPERCASE characters? Click OK for YES or click CANCEL for NO"
+    );
+    var includeLower = confirm(
+      "Would you like to include lowercase characters? Click OK for YES or click CANCEL for NO"
+    );
+    var includeNumbers = confirm(
+      "Would you like to include numeric characters? Click OK for YES or click CANCEL for NO"
+    );
+    var includeSpecial = confirm(
+      "Would you like to include special characters? Click OK for YES or click CANCEL for NO"
+    );
 
     console.log(includeUpper);
     console.log(includeLower);
     console.log(includeNumbers);
     console.log(includeSpecial);
-    
-    } else {
-      alert("Please enter a length between 8 and 128");
+
+    //put in if statement for true var in 16-19
+    if (includeUpper === true) {
+      characterSet = characterSet + upperChars;
     }
+
+    if (includeLower === true) {
+      characterSet = characterSet + lowerChars;
+    }
+
+    if (includeNumbers === true) {
+      characterSet = characterSet + numChars;
+    }
+
+    if (includeSpecial === true) {
+      characterSet = characterSet + specialChars
+          }
+
+    console.log(characterSet);
+  } else {
+    alert("Please enter a length between 8 and 128");
+  }
 }
 
 // Write password to the #password input
@@ -34,7 +65,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-//did this work to commit
+  //did this work to commit
 }
 
 // Add event listener to generate button
